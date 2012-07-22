3dd34e7f480d7ffbfb475cf98b9faca3bf1400e2
Jian <jian@Hephaestus.(none)>
2012-07-21 18:31:46 -0700

    models now support basic transactions

--- assassins/girl/model.py
+++ assassins/girl/model.py
@@ -1,25 +1,61 @@
 from django.db import models
 
 class Player(models.Model):
-    facebook_id = models.CharField(unique=True)
+    facebook_id = models.CharField(primary_key=True)
     access_token = models.CharField()
     first_name = models.CharField()
     last_name = models.CharField()
     gender = models.CharField()
-    photos = models.FileField()
-    location_lat = models.DecimalField()
-    location_long = models.DecimalField()
+    photo = models.CharField()
+    location_lat = models.FloatField()
+    location_long = models.FloatField()
+    session = models.ForeignKey(Session)
+    is_admin = models.BooleanField()
 
-class Game(models.Model):
-    game_id = models.IntegerField(unique=True)
-    game_name = models.CharField()
+    def add_player(self, uid, access_token, first_name, last_name, gender, photo, admin=False):
+        self.objects.create(facebook_id=uid,
+                            access_token=access_token,
+                            first_name=first_name,
+                            last_name=last_name,
+                            gender=gender,
+                            photo=photo,
+                            is_admin=admin)
 
-class Admins(models.Model):
-    facebook_id = models.ForeignKey(Player)
+    def update_location(self, uid, loc_lat, loc_long):
+        self.objects.get(facebook_id=uid).update(location_lat= loc_lat, location_long=loc_long)
+
+    def update_session(self, uid, session):
+        self.objects.get(facebook_id=uid).update(session=session)
+
+    def make_admin(self, uid):
+        self.objects.get(facebook_id=uid).update(is_admin=True)
+
+    def revoke_admin(self, uid):
+        self.objects.get(facebook_id=uid).update(is_admin=False)
+
+class Session(models.Model):
+    session_id = models.AutoField(primary_key=True)
+    session_name = models.CharField()
+
+    def add_session(self, session_name=None):
+        self.objects.create(session_name=session_name)
 
 class Feed(models.Model):
-    post_id = models.IntegerField(unique=True)
+    post_id = models.AutoField(primary_key=True)
+    session = models.ForeignKey(Session)
     from_user = models.ForeignKey(Player)
     to_user = models.ForeignKey(Player)
-    post = models.CharField()
+    message = models.CharField()
     create_time = models.IntegerField()
+
+    def post(self, message, from_user=None, to_user=None, session=None):
+        now = datetime.datetime.now()
+        now = time.mktime(now.timetuple())
+        self.objects.create(message=message,
+                            from_user=from_user, 
+                            to_user=to_user, 
+                            session=session,
+                            create_time=now)
+
+    def get_feed(self, limit, session=None):
+        return self.objects.all()[]
