d0bde4ba96e09e85dbecdb3c2be08fa7ef3c27bd
Jian <jian@Hephaestus.(none)>
2012-07-22 00:33:48 -0700

    fixed wrong object notations

--- assassins/views.py
+++ assassins/views.py
@@ -1,4 +1,4 @@
-Import simplejson
+import simplejson
 import urllib2
 
 from django.http import HttpRequest, HttpResponse
@@ -7,12 +7,23 @@ from django.template import RequestContext
 
 from girl.config import Config
 from models import *
+from util import *
+
+def report_kill(request):
+    assassin_id, lat, lng, target_id, tar_lat, tar_long = extract_location_data(request)
+    kill(assassin_id)
+    assassin_name = Assassin.get(facebook_id=assassin_id).get_first_name_display()
+    victim_name = Assassin.get(facebook_id=victim_id).get_first_name_display()
+    message = assassin_name+" just killed "+victim_name+"!"
+    post_to_feed(message, assassin_id, victim_name)
 
 def confirm_melee_kill(request):
     assassin_id, lat, lng, target_id, tar_lat, tar_long = extract_location_data(request)
     dist = get_distance(lat, lng, tar_lat, tar_long)
     if dist < Config.MAX_MELEE_KILL_DIST:
         Assassin.kill(assassin_id)
+        message = "MELEE! "+assassin_name+" just killed "+victim_name+"!"
+        post_to_feed(message, assassin_id, victim_name)
         return HttpResponse("kill")
     else:
         return HttpResponse("nokill")
@@ -24,7 +35,7 @@ def plant_bomb(request):
     assassin_id, lat, lng, target_id, tar_lat, tar_long = extract_location_data(request)
     bomb_type = request.GET['type']
     if bomb_type in ('sticky', 'mine'):
-        Bombs.add_bomb(assassin_id, target_id, bomb_type, lat, lng, seconds)
+        add_bomb(assassin_id, target_id, bomb_type, lat, lng, seconds)
 
 def extract_location_data(request):
     assassin_id = request.GET['fbid']
@@ -33,7 +44,7 @@ def extract_location_data(request):
     assassin = Assassin.objects.filter(facebook_id=assassin_id)
     target_id = assassin.get_target_id_display()
     target = Assassin.objects.filter(facebook_id=target_id)
-    tar_lat, tar_long = Assassin.get_location(target_id)
+    tar_lat, tar_long = get_location(target_id)
     return assassin_id, lat, lng, target_id, tar_lat, tar_long
 
 def get_distance(lat1, lng1, lat2, lng2):
@@ -57,12 +68,12 @@ def update_location(request):
     assassin_id = request.GET['fbid']
     lat = request.GET['lat']
     lng = request.GET['lng']
-    Assassin.update_location(assassin_id, lat, lng)
+    update_location(assassin_id, lat, lng)
 
 def new_game(request):
-    AssassinSession.add_session(request.GET['name'],
-                                request.GET['description'],
-                                request.GET['uid'])
+    add_session(request.GET['name'],
+                request.GET['description'],
+                request.GET['uid'])
 
 def add_new_player(request):
     facebook_id = request.GET['id']
@@ -77,7 +88,7 @@ def add_new_player(request):
 def add_player_to_game(request):
     player = Assassin.objects.filter(facebook_id=uid)
     game = AssassinSession.objects.filter(session_id=game_id)
-    Assassin.update_session(player, game)
+    update_session(player, game)
     
-def game_info(request):
-    pass
+def get_feed(request):
+    posts = Feed.get_limit(5)
